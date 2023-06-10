ee4c03f5de81c34a23cf9fa10d7c3b59bafb9ce9
Ibrahim Tangco <barangobong13@gmail.com>
2023-06-10 16:18:07 +0800

    change

--- asset/script/script.js
+++ asset/script/script.js
@@ -2,24 +2,23 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     event.preventDefault();
 
     //* Store the data from the form
-    const stream = document.getElementById("stream").value; // Get the value of the 'stream' input field
-    const pages = document.getElementById("page").value; // Get the value of the 'page' input field
-    const pagesSplitted = pages.split(" "); // Split the 'pages' value into an array of individual pages
-    const numFrames = parseInt(document.getElementById("frames").value); // Get the value of the 'frames' input field and parse it as an integer
-    const policy = document.getElementById("policy").value; // Get the value of the 'policy' input field
-    const pattern = /[a-zA-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/; // Regular expression pattern to match letters and special characters
-    let hasLetters = false; // Variable to track if the 'pages' input contains letters or special characters
+    const stream = document.getElementById("stream").value;
+    const pages = document.getElementById("page").value;
+    const pagesSplitted = pages.split(" ");
+    const numFrames = parseInt(document.getElementById("frames").value);
+    const policy = document.getElementById("policy").value;
+    const pattern = /[a-zA-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    let hasLetters = false;
 
     for (let i = 0; i < pagesSplitted.length; i++) {
         if (pattern.test(pagesSplitted[i])) {
-            hasLetters = true; // Set 'hasLetters' to true if any of the pages contain letters or special characters
+            hasLetters = true;
             break;
         }
     }
 
     //* Check if the inputted data is invalid
     if (!stream) {
-        // If 'stream' is empty
         Swal.fire({
             icon: "error",
             title: "Oops...",
@@ -30,7 +29,6 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
             }
         });
     } else if (hasLetters) {
-        // If 'pages' contain letters or special characters
         Swal.fire({
             icon: "error",
             title: "Oops...",
@@ -41,8 +39,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
             }
         });
     }
-
-    //* Create storage variables for the following
+    //* Create a storage for the following
     let pageFaults = 0;
     let pageHits = 0;
     let pageTable = [];
@@ -54,7 +51,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
 
     //* Check if the chosen policy is FIFO
     if (policy === "fifo") {
-        //* Store 'FIFO' as the value for 'algorithmName', this will be displayed in the result
+        //* Store 'FIFO' as a value for the algorithmName, this will be displayed in the result
         algorithmName = "FIFO";
 
         let oldestPageIndex = 0; // Track the index of the oldest page in the page table
@@ -83,7 +80,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
             //* Object for the references and frames
             let row = {
                 reference: page,
-                //* Store the array of pages as a string concatenated with a line break
+                //* Store the array of pages as a String concatenated with a line break
                 frames: pageTable.join("<br><hr>"),
             };
             //* Push the row object to the tableData array
@@ -186,10 +183,9 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     const pageHitRatio = (pageHits / pages.length) * 100;
     const pageFaultRatio = (pageFaults / pages.length) * 100;
 
-    // Assigning value to the output variable that will be displayed as the result
     output = `
     <h2 class="text-xl font-bold py-2">Summary - ${algorithmName} Algorithm</h2>
-  
+
     <ul class="pl-4 font-regular">
       <li class="list-disc py-px"><strong>Algorithm:</strong> ${algorithmName}</li>
       <li class="list-disc py-px"><strong>Reference Stream:</strong> ${stream}</li>
@@ -199,35 +195,43 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     </ul>
     <h2 class="text-xl font-bold pt-4">Solution Visualization</h2>
     <table class="w-full bg-myGray my-8">
-      <tbody>
-        <tr>
-          <th class="border text-center py-4 w-2/12">Reference</th>
-  
-          ${tableData
-              .map(
-                  (row) => `
-              <td class="border text-center py-4 font-bold">${row.reference}</td>
-          `
-              )
-              .join("")}
-        </tr>
-  
-        <tr>
-          <th class="border text-center py-4 w-2/12">Frames</th>
-  
-          ${tableData
-              .map(
-                  (row) => `
-              <td class="border text-center align-top py-4">
-                ${row.frames}
-              </td>
-          `
-              )
-              .join("")}
-        </tr>
-      </tbody>
-    </table>
-  
+   
+    <tbody>
+    <tr>
+        <th class="border text-center py-4 w-2/12">Reference</th>
+
+        ${tableData
+
+            .map(
+                (row) => `
+                
+                <td class="border text-center py-4 font-bold">${row.reference}</td>
+            
+        `
+            )
+            .join("")}
+    </tr>
+    
+    <tr>
+        <th class="border text-center py-4 w-2/12">Frames</th>
+
+        ${tableData
+            .map(
+                (row) => `
+                
+                <td class="border text-center align-top py-4">
+                  
+                        ${row.frames}
+                    
+                </td>
+            
+        `
+            )
+            .join("")}
+    </tr>
+    </tbody>
+</table>
+    
     <ul class="pl-4 font-regular">
       <li class="list-disc py-px"><strong>Total Page Hit:</strong> ${pageHits}</li>
       <li class="list-disc py-px"><strong>Total Page Fault:</strong> ${pageFaults}</li>
@@ -238,7 +242,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
         pages.length
     } = ${pageFaultRatio.toFixed(2)}%</li>
     </ul>
-    `;
+  `;
 
     // Store the output in the browser's local storage
     localStorage.setItem("output", output);
@@ -246,35 +250,3 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     // Redirect to the result.html page
     window.location.href = "result.html";
 });
-
-// The code defines an event listener for the "submit" event on an HTML form with the id "myForm".
-// When the form is submitted, the event listener function is executed.
-// The function prevents the default form submission behavior using event.preventDefault().
-
-// The code retrieves the values of various form elements such as "stream", "page", "frames", and "policy".
-
-// The code checks for invalid input by verifying if the "stream" value is empty or if the "pages" value contains any characters.
-
-// If the input is invalid, an error message is displayed using the Swal.fire() function from the SweetAlert library,
-// and the page is reloaded using location.reload().
-
-// If the input is valid, the code initializes variables to store page faults, page hits, page table, table data, algorithm name, and output.
-
-// The code then checks the chosen policy (FIFO, LRU, or LFU) and executes the corresponding code block.
-
-// The FIFO (First-In-First-Out) code block iterates through the pages and checks if each page is already in the page table.
-// If a page is present, it counts as a page hit; otherwise, it counts as a page fault.
-// If all frames are occupied, the oldest page is replaced with the new page.
-// The references and frames for each step are stored in an object, which is pushed to the tableData array.
-// After the iteration, the page hit ratio and page fault ratio are calculated.
-
-// The LRU (Least Recently Used) code block follows a similar logic as FIFO.
-// The page table is maintained in the order of most recently used to least recently used.
-// When a page is accessed, it is moved to the end of the page table, simulating its recent usage.
-
-// The LFU (Least Frequently Used) code block maintains a frequency count for each page using a Map.
-// When a page is accessed, its frequency is incremented.
-// When a page fault occurs and all frames are occupied, the page with the minimum frequency is replaced.
-// The references and frames are stored in tableData, and the page hit ratio and fault ratio are calculated.
-
-// Finally, the code generates the output HTML, stores it in the browser's local storage, and redirects to the result.html page.
