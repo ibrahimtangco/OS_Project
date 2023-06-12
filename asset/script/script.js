da3d9c3b3b7ecd1a3720bf4322c51eb1b39d701d
Ibrahim Tangco <barangobong13@gmail.com>
2023-06-12 22:22:26 +0800

    commit changes

--- asset/script/script.js
+++ asset/script/script.js
@@ -38,6 +38,13 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
             text: "Reference stream cannot contain characters",
         });
         return;
+    } else if (pageOld.length === 0) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Reference stream cannot be empty!",
+        });
+        return;
     }
     //* Create a storage for the following
     let pageFaults = 0;
@@ -46,7 +53,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     let tableData = [];
     let algorithmName = "";
     let output = "";
-
+    let pageFaultCounter = [];
     // TODO: FIFO code
 
     //* Check if the chosen policy is FIFO
@@ -65,8 +72,10 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
             //* If not, page fault will be incremented
             if (pageTable.includes(page)) {
                 pageHits++;
+                pageFaultCounter.push(0);
             } else {
                 pageFaults++;
+                pageFaultCounter.push(1);
 
                 //* If all frames are occupied, remove the oldest page from the page table
                 if (pageTable.length === numFrames) {
@@ -82,6 +91,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
                 reference: page,
                 //* Store the array of pages as a String concatenated with a line break
                 frames: pageTable.join("<br><hr>"),
+                faults: pageFaultCounter,
             };
             //* Push the row object to the tableData array
             tableData.push(row);
@@ -91,12 +101,14 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     // TODO: LRU code
     else if (policy === "lru") {
         algorithmName = "LRU";
+        const indexes = new Map(); // Map to store the indexes of pages
         for (let i = 0; i < pages.length; i++) {
             let page = parseInt(pages[i]);
 
             if (pageTable.includes(page)) {
                 // Page hit
                 pageHits++;
+                pageFaultCounter.push(0);
 
                 // Update the page's position in the pageTable
                 const pageIndex = pageTable.indexOf(page);
@@ -105,15 +117,29 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
             } else {
                 // Page fault
                 pageFaults++;
+                pageFaultCounter.push(1);
 
                 if (pageTable.length === numFrames) {
-                    // Remove the least recently used page
-                    pageTable.shift();
+                    // Find the page that was least recently used
+                    let leastRecentlyUsed = Infinity;
+                    let leastRecentlyUsedIndex;
+                    for (let j = 0; j < pageTable.length; j++) {
+                        if (indexes.get(pageTable[j]) < leastRecentlyUsed) {
+                            leastRecentlyUsed = indexes.get(pageTable[j]);
+                            leastRecentlyUsedIndex = j;
+                        }
+                    }
+
+                    // Replace the least recently used page with the new page
+                    pageTable.splice(leastRecentlyUsedIndex, 1);
                 }
 
                 // Add the new page to the pageTable
                 pageTable.push(page);
             }
+
+            // Update the index of the current page
+            indexes.set(page, i);
 
             let row = {
                 reference: page,
@@ -136,12 +162,14 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
             if (pageTable.includes(page)) {
                 // Page hit
                 pageHits++;
+                pageFaultCounter.push(0);
 
                 // Increment the page's frequency
                 pageFrequency.set(page, (pageFrequency.get(page) || 0) + 1);
             } else {
                 // Page fault
                 pageFaults++;
+                pageFaultCounter.push(1);
 
                 if (pageTable.length === numFrames) {
                     // Find the page with the minimum frequency
@@ -220,7 +248,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
                 (row) => `
                 
                 <td class="border text-center align-top py-4">
-                  
+                
                         ${row.frames}
                     
                 </td>
@@ -229,6 +257,28 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
             )
             .join("")}
     </tr>
+
+    <tr>
+    <th class="border text-center py-4 w-2/12">Hit</th>
+
+
+            
+            
+            ${pageFaultCounter.map((value) =>
+                value === 1
+                    ? `<td class="border text-center align-top py-4 bg-red-500 font-semibold text-white">
+                        <i class="fa-solid fa-xmark"></i>
+                    </td>`
+                    : `<td class="border text-center align-top py-4 bg-green-500 font-semibold text-white">
+                        <i class="fa-solid fa-check"></i>
+                    </td>`
+            )}
+
+
+
+        
+    
+</tr>
     </tbody>
 </table>
     
